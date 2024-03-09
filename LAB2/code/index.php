f662167dc28ef621ee7107fdd4157019715e63e3
GoshaLRG <9e0r9e1yam1n@gmail.com>
2024-03-09 14:41:35 +0200

    task 11
    Создали новые переменные num_languages, months, days - для подсчета дней ушедших на изучение каждого из языков, в переменной days_per_language

--- LAB2/code/index.php
+++ LAB2/code/index.php
@@ -1,26 +1,8 @@
 <?php
 
-$num1 = 20;
-$num2 = 10;
+$num_languages = 4;
+$months = 11;
+$days = $months * 16;
+$days_per_language = $days / $num_languages;
 
-echo $num1;
-echo "<br>"; 
-echo "\n"; 
-echo $num2;
-
-echo "<br>";
-echo "\n";
-
-$num3 = 2.28;
-echo $num3;
-echo "<br>";
-echo "\n";
-
-echo $num1 - $num2 + 2;
-echo "<br>";
-echo "\n";
-
-$last_month = 1187.23;
-$this_month = 1089.98;
-
-echo $last_month - $this_month;
+echo $days_per_language;
