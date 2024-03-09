569e5040013adfef26b6540afcab7c85fd6d5b56
GoshaLRG <9e0r9e1yam1n@gmail.com>
2024-03-09 14:18:24 +0200

    task1
    Объявил новую переменную $order как ссылку на переменную very_bad_unclear_name

--- LAB2/code/index.php
+++ LAB2/code/index.php
@@ -1,2 +1,8 @@
 <?php
-echo phpinfo();
+
+$very_bad_unclear_name = "15 chicken wings";
+
+$order = & $very_bad_unclear_name;
+$order .= "on a sesame bun";
+
+echo "\nYour order is:$very_bad_unclear_name.";
