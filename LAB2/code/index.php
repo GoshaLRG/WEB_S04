02603a0fbe0f174c00fe606fc9a7a42996e9fc21
GoshaLRG <9e0r9e1yam1n@gmail.com>
2024-03-20 21:56:53 +0200

    task 14 arr[]->arr()
    Исправил массивы

--- LAB2/code/index.php
+++ LAB2/code/index.php
@@ -1,67 +1,215 @@
 <?php
 
-$randArr = array();
-$randNum = rand(1, 10);
+// %
 
-for ($i = 0; $i < $randNum; $i++) {
-    $randArr[] = rand(1, 100);
+$a = 10;
+$b = 3;
+
+$rest = $a % $b;
+
+echo $rest;
+
+echo "<br>";
+
+echo "\n";
+
+if ($rest == 0) {
+
+    echo "Делится без остатка с результатом: ";
+
+    echo $a / $b;
+
 }
 
-foreach ($randArr as $elem) {
-    echo $elem . ' ';
+else {
+
+    echo "Делится с остатком: ";
+
+    echo $rest;
+
 }
 
 echo "<br>";
+
 echo "\n";
 
-$avValue = array_sum($randArr) / count($randArr);
-echo $avValue;
+// Cтепень и корень
+
+$st = pow(2,10);
+
+echo sqrt(245);
 
 echo "<br>";
+
 echo "\n";
 
-function ArithmProgress($x) {
-    return $x*($x + 1) / 2;
+
+
+
+$array = array(4,2,5,19,13,0,10);
+
+$sum = 0;
+
+foreach ($array as $value) {
+
+    $sum += pow($value, 2);
+
 }
 
-echo ArithmProgress(100);
+echo "Результат операций с массивом: " . sqrt($sum);
 
 echo "<br>";
+
 echo "\n";
 
-$randArr = array();
-$randNum = rand(1, 10);
+// округление
 
-for ($i = 0; $i < $randNum; $i++) {
-    $randArr[] = rand(1, 100);
+$num379 = sqrt(379);
+
+echo round($num379);
+
+echo "<br>";
+
+echo "\n";
+
+echo round($num379,1);
+
+echo "<br>";
+
+echo "\n";
+
+echo round($num379,2);
+
+echo "<br>";
+
+echo "\n";
+
+// округление до нижней и верхней границ
+
+$num587 = sqrt(587);
+
+$floorCeil = array(
+
+    "floor" => floor($num587),
+
+    "ceil" => ceil($num587)
+
+);
+
+// минимум и максимум
+
+$MinMax = array(4,-2,5,19,-130,0,10);
+
+echo min($MinMax);
+
+echo "<br>";
+
+echo "\n";
+
+echo max($MinMax);
+
+// рандом
+
+echo rand(1,100);
+
+for ($i = 0; $i < 10; $i++) {
+
+    $randArr[] = rand();
+
 }
 
-foreach ($randArr as $elem) {
-    echo $elem . ' ';
+foreach ($randArr as $value) {
+
+    echo $value . " ";
+
 }
 
 echo "<br>";
+
 echo "\n";
 
-$sqrtArr = array_map('sqrt', $randArr);
+// модуль
 
-foreach ($sqrtArr as $elem) {
-    echo $elem . ' ';
-}
+for ($i = 0; $i < 3; $i++) {
 
-echo "<br>";
-echo "\n";
+    $a = rand(1, 25);
 
-$keys = range('a', 'z');
-$values = range(1, 26);
-$key_valueArr = array_combine($keys, $values);
+    $b = rand(1, 25);
 
-foreach ($key_valueArr as $key => $value) {
-    echo "key: $key; value: $value";
+    echo abs($a - $b);
+
     echo "<br>";
+
     echo "\n";
+
 }
 
-$str = '1234567890';
-$sum = intval($str[0].$str[1]) + intval($str[2].$str[3]) + intval($str[4].$str[5]) + intval($str[6].$str[7]) + intval($str[8].$str[9]);
-echo $sum;
+$Arr = array(1,2,-1,-2,3,-3);
+
+$postArr = array();
+
+foreach ($Arr as $value) {
+
+    $postArr() = abs($value);
+
+}
+
+foreach ($postArr as $value) {
+
+    echo $value . " ";
+
+}
+
+echo "<br>";
+
+echo "\n";
+
+// общее:1
+
+$anyNum = 228;
+
+$anyArr = array();
+
+for ($i=1; $i <= 228; $i++) {
+
+    if (($anyNum % $i) == 0) {
+
+        $anyArr() = $i;
+
+    }
+
+}
+
+foreach ($anyArr as $value) {
+
+    echo $value . " ";
+
+}
+
+echo "<br>";
+
+echo "\n";
+
+// общее:2
+
+$sumArr = array(1,2,3,4,5,6,7,8,9,10);
+
+$sum10 = 0;
+
+$count = 0;
+
+foreach ($sumArr as $value) {
+
+    $sum10 += $value;
+
+    $count += 1;
+
+    if ($sum10 > 10) {
+
+        echo "Количество первых элементов массива: $count.";
+
+        break;
+
+    }
+
+}
