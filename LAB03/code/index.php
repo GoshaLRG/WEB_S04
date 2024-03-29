957288d7a35c6bbcd32ceb54c444a5991a31989e
GoshaLRG <9e0r9e1yam1n@gmail.com>
2024-03-29 22:37:16 +0200

    complete task 2.1

--- LAB03/code/index.php
+++ LAB03/code/index.php
@@ -1,25 +1,39 @@
 <?php
 
-// задание 1.1
-
-$string = 'ahb acb aeb aeeb adcb axeb';
-$regular = '/a..b/';
-preg_match_all($regular, $string, $matches);
-
-foreach($matches[0] as $i)
+if ($_SERVER['REQUEST_METHOD'] == 'POST') 
 {
-    echo $i."<br>";
+    $string = $_POST['text'];
+
+    $word_count = str_word_count($string);
+    $char_count = strlen($string);
+} 
+else 
+{
+    $string = '';
+    $word_count = 0;
+    $char_count = 0;
 }
 
-// задание 1.2
+?>
 
-$string = 'a1b2c3';
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Счетчик слов и символов</title>
+</head>
+<body>
+    <form method="post">
+        <textarea name="text"><?php echo $string; ?></textarea>
+        <br>
+        <button type="submit">Cосчитать слова и символы</button>
+    </form>
 
-function exponentiation($matches) 
-{
-    return pow($matches[0], 3);
-}
-
-$new_string = preg_replace_callback('/\d+/', 'exponentiation', $string);
-
-echo $new_string;
+    <?php if ($_SERVER['REQUEST_METHOD'] == 'POST'): ?>
+        <p>Количество слов: <?php echo $word_count; ?></p>
+        <p>Количество символов: <?php echo $char_count; ?></p>
+    <?php endif; ?>
+</body>
+</html>
