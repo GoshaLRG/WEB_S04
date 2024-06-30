05c19adf8c95f66442e3915e0ccab69db81e21bf
GoshaLRG <9e0r9e1yam1n@gmail.com>
2024-06-30 14:49:33 +0200

    Connect to mysql

---
+++ LAB5/code/index.php
@@ -0,0 +1,7 @@
+<?php
+$mysqli = new mysqli('db', 'root', 'helloworld', 'web');
+
+if (mysqli_connect_errno()) {
+    printf("Подключение к серверу MySQL невозможно. Код ошибки: %s\n", mysqli_connect_error());
+    exit;
+}
